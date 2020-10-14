407d480f8d70d68429fb7c93d30a53910bcb80fe
zhangwei <1026735335@qq.com>
2020-10-14 21:33:41 +0800

    表单数据整合

--- src/views/carte.vue
+++ src/views/carte.vue
@@ -7,91 +7,89 @@
  * @LastEditTime: 2020-10-12 18:24:17
 -->
 <template>
-  <div class="crte">
+<div class="crte">
     <header>
-      <div class="top">
-        <h3>查询条件</h3>
-        <el-button size="mini"
-                   @click="isSHow = !isSHow"
-                   round>查询</el-button>
-      </div>
-      <transition name="el-zoom-in-top">
-        <div class="bottom"
-             v-show="isSHow">
-          <span>菜单名称：</span>
-          <el-input></el-input>
+        <div class="top">
+            <h3>查询条件</h3>
+            <el-button size="mini" @click="isSHow = !isSHow" round>查询</el-button>
         </div>
-      </transition>
-      <div class="footerInfo">
-        <el-button size="mini"
-        @click="nowNum =2"
-                   round>添加报表目录</el-button>
-        <el-button size="mini"
-        @click="nowNum = 1"
-                   round>添加报表</el-button>
-        <el-button size="mini"
-                   round>删除</el-button>
-      </div>
+        <transition name="el-zoom-in-top">
+            <div class="bottom" v-show="isSHow">
+                <span>菜单名称：</span>
+                <el-input></el-input>
+            </div>
+        </transition>
+        <div class="footerInfo">
+            <el-button size="mini" @click="nowNum =2" round>添加报表目录</el-button>
+            <el-button size="mini" @click="nowNum = 1" round>添加报表</el-button>
+            <el-button size="mini" round>删除</el-button>
+        </div>
     </header>
     <el-container>
-      <el-aside width="200px">
-        <tree />
-      </el-aside>
-      <el-main>
-        <add-catalogue v-if="nowNum === 1"/>
-        <addReport v-if="nowNum === 2" />
-      </el-main>
+        <el-aside width="200px">
+            <tree />
+        </el-aside>
+        <el-main>
+            <add-catalogue v-if="nowNum === 1" />
+            <addReport v-if="nowNum === 2" />
+        </el-main>
     </el-container>
-  </div>
+</div>
 </template>
+
 <script>
 import addCatalogue from 'components/addCatalogue'
 import addReport from 'components/addReport'
 import tree from './tree'
 export default {
-  name: 'crte',
-  components: {
-    addCatalogue,
-    addReport,
-    tree,
-  },
-  data() {
-    return {
-      isSHow: false,
-      nowNum: 0
-    }
-  },
-  methods: {},
+    name: 'crte',
+    components: {
+        addCatalogue,
+        addReport,
+        tree,
+    },
+    data() {
+        return {
+            isSHow: false,
+            nowNum: 0
+        }
+    },
+    methods: {},
 }
 </script>
 
-<style lang='less' scoped>
+<style lang="less" scoped>
 .crte {
-  > header {
-    line-height: normal;
-    padding: 10px 0;
-    > .top {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 1px solid #dedede;
-      padding-bottom: 10px;
-      margin-bottom: 10px;
+    >header {
+        line-height: normal;
+        padding: 10px 0;
+
+        >.top {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 1px solid #dedede;
+            padding-bottom: 10px;
+            margin-bottom: 10px;
+        }
+
+        >.bottom {
+            margin-bottom: 15px;
+            display: flex;
+
+            >span {
+                width: 100px;
+            }
+
+            align-items: center;
+            margin: 15px 0;
+        }
+
+        >.footerInfo {
+            margin-bottom: 15px;
+            border-bottom: 1px solid #dedede;
+            padding-bottom: 15px;
+        }
     }
-    > .bottom {
-      margin-bottom: 15px;
-      display: flex;
-      > span {
-        width: 100px;
-      }
-      align-items: center;
-      margin: 15px 0;
-    }
-    > .footerInfo {
-      margin-bottom: 15px;
-      border-bottom: 1px solid #dedede;
-      padding-bottom: 15px;
-    }
-  }
 }
 </style>
--- src/views/rule/index.vue
+++ src/views/rule/index.vue
@@ -1,181 +1,192 @@
 <template>
-  <div class="page" @click="pageClick">
+<div class="page" @click="pageClick">
     <div class="navLeft" @contextmenu.prevent.stop="(e) => show(e, 0)">
-      <el-menu default-active="2" class="el-menu-vertical-demo">
-        <el-submenu index="1">
-          <template slot="title">
-            <div
-              class="menaReference"
-              slot="reference"
-              @contextmenu.prevent.stop="(e) => show(e, 1)"
-            >
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </div>
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-      </el-menu>
+        <el-menu default-active="2" class="el-menu-vertical-demo" background-color='#FFFFFF'>
+            <el-submenu index="1">
+                <template slot="title">
+                    <div class="menaReference" slot="reference" @contextmenu.prevent.stop="(e) => show(e, 1)">
+                        <i class="el-icon-location"></i>
+                        <span>导航一</span>
+                    </div>
+                </template>
+                <el-menu-item-group>
+                    <template slot="title">分组一</template>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+        </el-menu>
     </div>
     <div class="content">
-      <div class="views">
-        <router-view />
-      </div>
+        <div class="views">
+            <router-view />
+        </div>
     </div>
     <div class="popoverBox" id="popoverBoxNav" v-show="popoverBox">
-      <div v-if="popoverBoxType !== 0" class="cell" @click="addCatalog(0)">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#iconicon_tianjia"></use>
-        </svg>
-        新建指标
-      </div>
-      <div class="cell" @click="addCatalog(1)">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#iconicon_tianjia"></use>
-        </svg>
-        新建目录
-      </div>
-      <div v-if="popoverBoxType !== 0" class="cell">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#iconicon_tianjia"></use>
-        </svg>
-        删除
-      </div>
+        <div v-if="popoverBoxType !== 0" class="cell" @click="addCatalog(0)">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#iconicon_tianjia"></use>
+            </svg>
+            新建指标
+        </div>
+        <div class="cell" @click="addCatalog(1)">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#iconicon_tianjia"></use>
+            </svg>
+            新建目录
+        </div>
+        <div v-if="popoverBoxType !== 0" class="cell">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#iconicon_tianjia"></use>
+            </svg>
+            删除
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "Rule",
-  data() {
-    return {
-      popoverBoxType: 0,
-      popoverBox: false,
-      activeName: "second",
-    };
-  },
-  mounted() {},
-  methods: {
-    // 全屏收起弹出框
-    pageClick() {
-      this.popoverBox = false;
+    name: "Rule",
+    data() {
+        return {
+            popoverBoxType: 0,
+            popoverBox: false,
+            activeName: "second",
+        };
     },
-    // 展示弹出框
-    show(e, type) {
-      /**
-       * 0: 在空白处右键
-       * 1: 在目录上右键
-       * 2: 在指标上右键
-       */
-      this.popoverBoxType = type;
-      const { pageX, pageY } = e;
-      const popoverBox = document.getElementById("popoverBoxNav");
-      popoverBox.style.top = pageY + "px";
-      popoverBox.style.left = pageX + "px";
-      this.popoverBox = true;
+    mounted() {},
+    methods: {
+        // 全屏收起弹出框
+        pageClick() {
+            this.popoverBox = false;
+        },
+        // 展示弹出框
+        show(e, type) {
+            /**
+             * 0: 在空白处右键
+             * 1: 在目录上右键
+             * 2: 在指标上右键
+             */
+            this.popoverBoxType = type;
+            const {
+                pageX,
+                pageY
+            } = e;
+            const popoverBox = document.getElementById("popoverBoxNav");
+            popoverBox.style.top = pageY + "px";
+            popoverBox.style.left = pageX + "px";
+            this.popoverBox = true;
+        },
+        // 新增目录
+        addCatalog(type) {
+            let name = "指标";
+            if (type === 1) {
+                name = "目录";
+            }
+            this.$prompt(`请输入${name}名称`, `创建${name}`, {
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                })
+                .then(({
+                    value
+                }) => {
+                    console.log(value);
+                })
+                .catch(() => {
+                    this.$message({
+                        type: "info",
+                        message: "取消创建",
+                    });
+                });
+        },
+        // handleOpen(key, keyPath) {
+        //   console.log(key, keyPath);
+        // },
+        // handleClose(key, keyPath) {
+        //   console.log(key, keyPath);
+        // },
     },
-    // 新增目录
-    addCatalog(type) {
-      let name = "指标";
-      if (type === 1) {
-        name = "目录";
-      }
-      this.$prompt(`请输入${name}名称`, `创建${name}`, {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-      })
-        .then(({ value }) => {
-          console.log(value);
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: "取消创建",
-          });
-        });
-    },
-    // handleOpen(key, keyPath) {
-    //   console.log(key, keyPath);
-    // },
-    // handleClose(key, keyPath) {
-    //   console.log(key, keyPath);
-    // },
-  },
 };
 </script>
 
-<style scoped lang="less">
+<style lang="less" scoped>
 .popoverBox {
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  height: auto;
-  width: 150px;
-  border-radius: 10px;
-  overflow: hidden;
-  box-shadow: 0 0 10px #999999;
-  border-radius: 5px;
-  background-color: white;
-  z-index: 1000;
-  cursor: pointer;
-  .cell {
-    width: 100%;
-    padding: 5px 10px;
-    box-sizing: border-box;
-    background-color: white;
-    svg {
-      filter: drop-shadow(#000000 80px 0);
-      transform: translateX(-80px);
-      overflow: hidden;
-    }
-  }
-  .cell:hover {
-    svg {
-      filter: drop-shadow(white 80px 0);
-      transform: translateX(-80px);
-      overflow: hidden;
-    }
-    color: white;
-    background-color: #5eaaff;
-  }
-}
-.menaReference {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  outline: none;
-}
-.popoverBoxHeader {
-  width: 100%;
-  height: 63px;
-}
-.page {
-  display: flex;
-  height: 100%;
-  .content {
-    background: white;
-    flex: 1;
-  }
-  .navLeft {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    height: auto;
+    width: 150px;
+    border-radius: 10px;
     overflow: hidden;
-    margin-right: 6px;
-    height: 100%;
-    flex: 0 0 200px;
-    background: white;
-    /deep/ .el-submenu__title::marker {
-      background-color: red;
+    box-shadow: 0 0 10px #999999;
+    border-radius: 5px;
+    background-color: white;
+    z-index: 1000;
+    cursor: pointer;
+
+    .cell {
+        width: 100%;
+        padding: 5px 10px;
+        box-sizing: border-box;
+        background-color: white;
+
+        svg {
+            filter: drop-shadow(#000000 80px 0);
+            transform: translateX(-80px);
+            overflow: hidden;
+        }
     }
-  }
+
+    .cell:hover {
+        svg {
+            filter: drop-shadow(white 80px 0);
+            transform: translateX(-80px);
+            overflow: hidden;
+        }
+
+        color: white;
+        background-color: #5eaaff;
+    }
+}
+
+.menaReference {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    outline: none;
+}
+
+.popoverBoxHeader {
+    width: 100%;
+    height: 63px;
+}
+
+.page {
+    display: flex;
+    height: 100%;
+
+    .content {
+        background: white;
+        flex: 1;
+    }
+
+    .navLeft {
+        overflow: hidden;
+        margin-right: 6px;
+        height: 100%;
+        flex: 0 0 200px;
+        background: white;
+
+        /deep/ .el-submenu__title::marker {
+            background-color: red;
+        }
+    }
 }
 </style>
